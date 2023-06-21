074590b32b753f6c3caf2e380a73b773d2fd3e3d
Austin Varghese <avargh01@uoguelph.ca>
2023-06-21 00:02:42 -0400

    created mobile hamburger menu and updated navbar, updated file structure and added hooks to store dark/light mode data in local storage and read from it

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 // import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+// import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // const [darkMode, setDarkMode] = useState(false)
+  return <>
+    <Navbar/>
+    <Component {...pageProps} />
+  </>
 }
