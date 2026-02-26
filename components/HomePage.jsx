ec73b7895de2cbc54203aec3f557e5881f437891
Austin-FV <austin.varghese7@gmail.com>
2026-02-26 00:29:57 -0500

    updated homepage and resume

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -40,8 +40,7 @@ const HomePage = () => {
                         <div className='text-center p-10'>
                             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Austin Varghese</h2>
                             <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-100'>Software Developer</h3>
-                            <p className='text-md py-3 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-300'>I am a Software Engineering Graduate from the University of Guelph with a minor in Business. I have a passion for designing and developing code. This includes experience with technologies such as
-                                React, Node.js, SQL, Unity, AWS, Azure, GCP and more.
+                            <p className='text-md py-3 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-300'>I am a Software Engineering Graduate from the University of Guelph with a minor in Business. I have a passion for designing and developing code. When I’m not programming, I’m probably at the gym or learning about new tech online. Big Marvel, anime, and gaming fan too.
                             </p>
                         </div>
                         <div className='text-5xl flex justify-center gap-16 pb-8 text-gray-600 md:hidden'>
